c2e4970c4d6e3e53a134d0eafde02e084fa81cc1
MH4GF <h.miyagi.cnw@gmail.com>
2023-01-17 15:46:37 +0900

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { useQuery } from '@apollo/client';
-import { graphql } from './gql/gql'; // 生成されたコードから `graphql()` をimport
-
-import Film from './Film';
+import { useQuery } from "@apollo/client";
+import { graphql } from "./gql/gql"; // 生成されたコードから `graphql()` をimport
+import Film from "./Film";
 
 // GraphQLで取得したい内容を定義
 // ここで定義した内容はTypedDocumentNode(後述)として型付けされる
@@ -20,10 +19,18 @@ const allFilmsWithVariablesQueryDocument = graphql(/* GraphQL */ `
 function App() {
   // ほとんどのGraphQLクライアントはTypedDocumentNodeを扱う方法を知っている
   // ドキュメントをuseQueryに渡すだけで、返却されるdataや第二引数で渡すvariablesも型付けがされている！
-  const { data } = useQuery(allFilmsWithVariablesQueryDocument, { variables: { first: 10 } });
+  const { data } = useQuery(allFilmsWithVariablesQueryDocument, {
+    variables: { first: 10 },
+  });
   return (
     <div className="App">
-      {data && <ul>{data.allFilms?.edges?.map((e, i) => e?.node && <Film film={e?.node} key={`film-${i}`} />)}</ul>}
+      {data && (
+        <ul>
+          {data.allFilms?.edges?.map(
+            (e, i) => e?.node && <Film film={e?.node} key={`film-${i}`} />
+          )}
+        </ul>
+      )}
     </div>
   );
 }
